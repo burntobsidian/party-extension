c88468fbed7dba85b74255e6dd4f4558489fa2a1
burntobsidian <me@burntobsidian.com>
2023-10-29 19:00:37 +0530

    cleanup server

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,17 +13,10 @@ export default class Server implements Party.Server {
   room: ${this.party.id}
   url: ${new URL(ctx.request.url).pathname}`
     );
-    // conn.setState()
-    // let's send a message to the connection
-    // conn.send("hello from server");
   }
 
   onMessage(message: string, sender: Party.Connection) {
-    // if (sender.state && "hostname" in sender.state && message === sender.state.hostname) return;
-
     if (message === "keepalive") {
-      console.log(message, sender.id, new Date().getSeconds());
-
       return;
     }
 
@@ -34,42 +27,26 @@ export default class Server implements Party.Server {
 
     const liveMembers = (this.partyMembers[message] ?? 0) + 1;
     this.partyMembers[message] = liveMembers;
+
     let previousHostname;
     let previousLiveMembers;
     if (sender.state && "hostname" in sender.state && typeof sender.state.hostname === "string") {
-      // console.log(sender.state.hostname);
       previousHostname = sender.state.hostname;
       previousLiveMembers = this.partyMembers[previousHostname] - 1;
       this.partyMembers[previousHostname] = previousLiveMembers;
     }
 
     sender.setState({ hostname: message });
-    // let's log the message
-    // console.log(`connection ${sender.id} sent message: ${message}`);
-    // as well as broadcast it to all the other connections in the room...
-    console.log([...this.party.getConnections()].length);
 
     for (const connection of this.party.getConnections()) {
-      console.log("state", connection.state);
+      if (!(connection.state && "hostname" in connection.state)) return;
 
-      if (
-        connection.state &&
-        "hostname" in connection.state &&
-        connection.state.hostname === previousHostname
-      ) {
-        console.log("conns", connection, previousLiveMembers);
+      if (connection.state.hostname === previousHostname) {
         connection.send(String(previousLiveMembers));
-      } else if (
-        connection.state &&
-        "hostname" in connection.state &&
-        connection.state.hostname === message
-      ) {
-        console.log("conns", connection, liveMembers);
+      } else if (connection.state.hostname === message) {
         connection.send(String(liveMembers));
       }
     }
-
-    console.log(this.partyMembers);
   }
 
   onClose(connection: Party.Connection<unknown>): void | Promise<void> {
